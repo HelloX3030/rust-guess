c66d669e0732f8431e5708b292cc8e553b02df41
lseeger <lseeger@student.42.fr>
2025-07-23 15:21:43 +0200

    added guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+        println!("Please input your guess:");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess = guess.trim();
+        if guess == "exit" { break; }
+
+        println!("You guessed: \"{}\"", guess);
+
+        let guess: u32 = match guess.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("pls enter a number!");
+                continue;
+            }
+        };
+
+        if guess < secret_number {
+            println!("Too small!");
+        } else if guess > secret_number {
+            println!("Too big!");
+        } else {
+            println!("You win!");
+            break;
+        }
+    }
 }
